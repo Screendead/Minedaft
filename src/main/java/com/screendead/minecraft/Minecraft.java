c8f47543a58e6626591078217339d81add446815
Jack Lusher <admin@screendead.com>
2018-10-01 04:03:40 +0100

    Render a textured quad

--- src/main/java/com/screendead/minecraft/Minecraft.java
+++ src/main/java/com/screendead/minecraft/Minecraft.java
@@ -4,12 +4,14 @@ import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 import org.lwjgl.system.*;
+import org.lwjgl.stb.*;
 
 import java.nio.*;
 
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL12.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
@@ -46,7 +48,7 @@ public class Minecraft {
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
 
         // Create the window
-        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+        window = glfwCreateWindow(800, 800, "Hello World!", NULL, NULL);
         if ( window == NULL )
             throw new RuntimeException("Failed to create the GLFW window");
 
@@ -92,15 +94,71 @@ public class Minecraft {
         // bindings available for use.
         GL.createCapabilities();
 
+        // Enable 2D texturing
+        glEnable(GL_TEXTURE_2D);
+
         // Set the clear color
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        // Get an image for texturing
+        IntBuffer w = BufferUtils.createIntBuffer(1),
+                h = BufferUtils.createIntBuffer(1),
+                channels = BufferUtils.createIntBuffer(1);
+        STBImage.stbi_set_flip_vertically_on_load(true);
+        ByteBuffer img = STBImage.stbi_load("C:/Users/admin/Documents/IntelliJ IDEA Projects/Minecraft/res/img/heart.png", w, h, channels, 4);
+        if (img == null) throw new RuntimeException("Failed to load texture.");
+
+        int width = w.get(),
+                height = h.get();
+        int id = glGenTextures();
+        if (id == 0) throw new RuntimeException("Failed to allocate texture ID.");
+
+        // Bind the texture
+        glBindTexture(GL_TEXTURE_2D, id);
+
+        // Set texture parameters
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        // Flip the buffer for reading
+        img.flip();
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img);
+
+        // Unbind the texture
+        glBindTexture(GL_TEXTURE_2D, 0);
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
         while ( !glfwWindowShouldClose(window) ) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
 
-            glfwSwapBuffers(window); // swap the color buffers
+            // Bind the texture
+            glBindTexture(GL_TEXTURE_2D, id);
+
+            // Draw a quad to the framebuffer
+//            glColor3f(1.0f, 0.0f, 0.5f);
+            glBegin(GL_QUADS);
+                glTexCoord2f(0.0f, 0.0f);
+                glVertex2f(-0.5f, -0.5f);
+
+                glTexCoord2f(0.0f, 1.0f);
+                glVertex2f(-0.5f,  0.5f);
+
+                glTexCoord2f(1.0f, 1.0f);
+                glVertex2f( 0.5f,  0.5f);
+
+                glTexCoord2f(1.0f, 0.0f);
+                glVertex2f( 0.5f, -0.5f);
+            glEnd();
+
+            // Draw buffer to the screen
+            glfwSwapBuffers(window);
+
+            // Unbind the texture
+            glBindTexture(GL_TEXTURE_2D, 0);
 
             // Poll for window events. The key callback above will only be
             // invoked during this call.
